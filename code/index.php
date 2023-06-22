307285124473f60bcd4c49497c110c45d16b1c2f
Chris Rickard <103567821+rickardc@users.noreply.github.com>
2023-06-22 11:29:17 +1000

    intial

---
+++ code/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Code</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <script src="index.js"></script>
+</head>
+<body>
+    <h1>Code</h1>
+    <p>Code</p>
+</body>
+</html>
+```
+I have tried to use the following code in the .htaccess file:
+```
